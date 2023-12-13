2eee5b3f353ae6c3d443b2323491e58f762c27e1
AdityaChowdhury2 <adityac5102@gmail.com>
2023-12-13 19:48:20 +0600

    user profile updated

--- src/models/Users.js
+++ src/models/Users.js
@@ -5,6 +5,14 @@ const UserSchema = new Schema({
     displayName: String,
     email: String,
     uid: String,
+    name: String,
+    address: String,
+    bio: String,
+    city: String,
+    landmark: String,
+    userName: String,
+    website: String,
+    zip: String,
     cart: [{ productId: Number, quantity: Number }]
 })
 
